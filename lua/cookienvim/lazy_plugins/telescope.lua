eccf19c751d38ad0b0d406a68accb9f975a8ff18
Harrison Cook <HonsonCooky@gmail.com>
2023-07-08 13:13:56 +1200

    implement nvchad style telescope preview

--- lua/cookienvim/lazy_plugins/telescope.lua
+++ lua/cookienvim/lazy_plugins/telescope.lua
@@ -10,7 +10,33 @@ return {
   },
   config = function()
     local telescope = require("telescope")
-    telescope.setup {}
+    local actions = require("telescope.actions")
+    telescope.setup {
+      defaults = {
+        mappings = {
+          i = {
+            ["<esc>"] = actions.close
+          },
+        },
+        initial_mode = "insert",
+        selection_strategy = "reset",
+        sorting_strategy = "ascending",
+        layout_strategy = "horizontal",
+        layout_config = {
+          horizontal = {
+            prompt_position = "top",
+            preview_width = 0.55,
+            results_width = 0.8,
+          },
+          vertical = {
+            mirror = false,
+          },
+          width = 0.87,
+          height = 0.80,
+          preview_cutoff = 120,
+        },
+      }
+    }
     telescope.load_extension("harpoon")
     pcall(require('telescope').load_extension, 'fzf')
   end
