980e7c357c60e42ccc94c5f311aec55885aa818d
Harrison Cook <HonsonCooky@gmail.com>
2023-08-14 18:15:28 +1200

    fix auto-dark lazy load

--- lua/cookienvim/lazy_plugins/autodarkmode.lua
+++ lua/cookienvim/lazy_plugins/autodarkmode.lua
@@ -1,13 +1,12 @@
-local thememod = require("cookienvim.thememod")
 return {
     "f-person/auto-dark-mode.nvim",
     config = {
         update_interval = 1000,
         set_dark_mode = function()
-            thememod.align_theme(BackgroundOptions.DARK)
+            require("cookienvim.thememod").align_theme(BackgroundOptions.DARK)
         end,
         set_light_mode = function()
-            thememod.align_theme(BackgroundOptions.LIGHT)
+            require("cookienvim.thememod").align_theme(BackgroundOptions.LIGHT)
         end,
     },
 }
