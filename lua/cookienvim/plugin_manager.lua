16d8a6ef0b29f478eb3566ec37e209ffef4af8b6
Harrison Cook <HonsonCooky@gmail.com>
2024-05-04 01:26:07 +1200

    remove UI features no longer needed

--- lua/cookienvim/plugin_manager.lua
+++ lua/cookienvim/plugin_manager.lua
@@ -7,72 +7,69 @@ to package listing, to make it easier to read, but you must include dependencies
 --[[Setup and Install Lazy.nvim if it doesn't exist on the system]]
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not vim.loop.fs_stat(lazypath) then
-  vim.fn.system({
-    "git",
-    "clone",
-    "--filter=blob:none",
-    "https://github.com/folke/lazy.nvim.git",
-    "--branch=stable", -- latest stable release
-    lazypath,
-  })
+	vim.fn.system({
+		"git",
+		"clone",
+		"--filter=blob:none",
+		"https://github.com/folke/lazy.nvim.git",
+		"--branch=stable", -- latest stable release
+		lazypath,
+	})
 end
 vim.opt.rtp:prepend(lazypath)
 
 --[[List of external packages]]
 require("lazy").setup({
-  --[[Basic Assistance]]
-  "windwp/nvim-autopairs",
-  "windwp/nvim-ts-autotag",
-  "numToStr/Comment.nvim",
-  "folke/which-key.nvim",
+	--[[Basic Assistance]]
+	"windwp/nvim-autopairs",
+	"windwp/nvim-ts-autotag",
+	"numToStr/Comment.nvim",
+	"folke/which-key.nvim",
 
-  --[[Dependancies]]
-  "nvim-tree/nvim-web-devicons",
-  "MunifTanjim/nui.nvim",
-  "3rd/image.nvim",
-  "nvim-lua/plenary.nvim",
+	--[[Dependancies]]
+	"nvim-tree/nvim-web-devicons",
+	"MunifTanjim/nui.nvim",
+	"3rd/image.nvim",
+	"nvim-lua/plenary.nvim",
 
-  --[[File Management]]
-  "nvim-neo-tree/neo-tree.nvim",
-  "nvim-telescope/telescope.nvim",
-  "nvim-telescope/telescope-fzf-native.nvim",
-  "nvim-telescope/telescope-ui-select.nvim",
+	--[[File Management]]
+	"nvim-telescope/telescope.nvim",
+	"nvim-telescope/telescope-fzf-native.nvim",
+	"nvim-telescope/telescope-ui-select.nvim",
 
-  --[[Git]]
-  "lewis6991/gitsigns.nvim",
-  "tpope/vim-fugitive",
-  "tpope/vim-rhubarb",
+	--[[Git]]
+	"lewis6991/gitsigns.nvim",
+	"tpope/vim-fugitive",
+	"tpope/vim-rhubarb",
 
-  --[[Language Assistance]]
-  "neovim/nvim-lspconfig",
-  "folke/neodev.nvim",
-  "echasnovski/mini.nvim",
-  -- Language Assistance: AutoCompletion
-  "hrsh7th/nvim-cmp",
-  "hrsh7th/cmp-nvim-lsp",
-  "hrsh7th/cmp-buffer",
-  "hrsh7th/cmp-path",
-  "hrsh7th/cmp-cmdline",
-  "rafamadriz/friendly-snippets",
-  "L3MON4D3/LuaSnip",
-  "saadparwaiz1/cmp_luasnip",
-  -- Language Assistance: Builtin Installer
-  "williamboman/mason.nvim",
-  "williamboman/mason-lspconfig.nvim",
-  -- Lanaguge Assistance: Linting and Formatting
-  "nvimtools/none-ls.nvim",
-  "jay-babu/mason-null-ls.nvim",
-  -- Language Assistance: Treesitter
-  "nvim-treesitter/nvim-treesitter",
-  "nvim-treesitter/nvim-treesitter-context",
-  "nvim-treesitter/nvim-treesitter-refactor",
-  "nvim-treesitter/nvim-treesitter-textobjects",
+	--[[Language Assistance]]
+	"neovim/nvim-lspconfig",
+	"folke/neodev.nvim",
+	"echasnovski/mini.nvim",
+	-- Language Assistance: AutoCompletion
+	"hrsh7th/nvim-cmp",
+	"hrsh7th/cmp-nvim-lsp",
+	"hrsh7th/cmp-buffer",
+	"hrsh7th/cmp-path",
+	"hrsh7th/cmp-cmdline",
+	"rafamadriz/friendly-snippets",
+	"L3MON4D3/LuaSnip",
+	"saadparwaiz1/cmp_luasnip",
+	-- Language Assistance: Builtin Installer
+	"williamboman/mason.nvim",
+	"williamboman/mason-lspconfig.nvim",
+	-- Lanaguge Assistance: Linting and Formatting
+	"nvimtools/none-ls.nvim",
+	"jay-babu/mason-null-ls.nvim",
+	-- Language Assistance: Treesitter
+	"nvim-treesitter/nvim-treesitter",
+	"nvim-treesitter/nvim-treesitter-context",
+	"nvim-treesitter/nvim-treesitter-refactor",
+	"nvim-treesitter/nvim-treesitter-textobjects",
 
-  --[[User Interface]]
-  "f-person/auto-dark-mode.nvim",
-  -- "catppuccin/nvim",
-  "rose-pine/neovim",
-  "nvim-lualine/lualine.nvim",
-  "arkav/lualine-lsp-progress",
-  "akinsho/toggleterm.nvim",
+	--[[User Interface]]
+	"f-person/auto-dark-mode.nvim",
+	"rose-pine/neovim",
+	"nvim-lualine/lualine.nvim",
+	"arkav/lualine-lsp-progress",
 })
--- lua/cookienvim/user_interface.lua
+++ lua/cookienvim/user_interface.lua
@@ -8,15 +8,15 @@ vim.cmd.colorscheme("rose-pine")
 
 --[[AutoDarkMode]]
 require("auto-dark-mode").setup({
-  update_interval = 1000,
-  set_dark_mode = function()
-    vim.o.background = "dark"
-    vim.cmd("mode")
-  end,
-  set_light_mode = function()
-    vim.o.background = "light"
-    vim.cmd("mode")
-  end,
+	update_interval = 1000,
+	set_dark_mode = function()
+		vim.o.background = "dark"
+		vim.cmd("mode")
+	end,
+	set_light_mode = function()
+		vim.o.background = "light"
+		vim.cmd("mode")
+	end,
 })
 
 --[[Fonts]]
@@ -26,118 +26,65 @@ local max_size = 20
 local min_size = 4
 
 local function font_setup()
-  vim.opt.guifont = font_family .. ":h" .. font_size
+	vim.opt.guifont = font_family .. ":h" .. font_size
 end
 
 function Font_Increase()
-  local prefix_count = math.max(vim.v.count, 1)
-  font_size = math.min(font_size + prefix_count, max_size)
-  font_setup()
+	local prefix_count = math.max(vim.v.count, 1)
+	font_size = math.min(font_size + prefix_count, max_size)
+	font_setup()
 end
 
 function Font_Decrease()
-  local prefix_count = math.max(vim.v.count, 1)
-  font_size = math.max(font_size - prefix_count, min_size)
-  font_setup()
+	local prefix_count = math.max(vim.v.count, 1)
+	font_size = math.max(font_size - prefix_count, min_size)
+	font_setup()
 end
 
 font_setup()
 
 --[[LuaLine]]
-require("lualine").setup({
-  sections = {
-    lualine_a = { "mode" },
-    lualine_b = { "branch" },
-    lualine_c = {
-      "diff",
-      "diagnostics",
-      "lsp_progress",
-    },
-    lualine_x = { "encoding", "fileformat", "filetype" },
-    lualine_y = { "progress" },
-    lualine_z = { "location" },
-  },
-  tabline = {
-    lualine_a = { "tabs" },
-    lualine_b = { "buffers" },
-    lualine_c = {},
-    lualine_x = {},
-    lualine_y = {},
-    lualine_z = {},
-  },
-})
-
---[[NeoTree]]
-require("neo-tree").setup({
-  filesystem = {
-    filtered_items = {
-      visible = true,
-      always_show = {
-        ".gitignored",
-      },
-    },
-  },
-})
+require("lualine").setup()
 
 --[[TeleScope]]
 local ts = require("telescope")
 ts.setup({
-  defaults = {
-    initial_mode = "normal",
-    sorting_strategy = "ascending",
-    layout_config = {
-      horizontal = {
-        prompt_position = "top",
-        preview_width = 0.55,
-        results_width = 0.8,
-      },
-      vertical = {
-        mirror = false,
-      },
-      width = 0.87,
-      height = 0.80,
-      preview_cutoff = 120,
-    },
-  },
+	defaults = {
+		initial_mode = "normal",
+		sorting_strategy = "ascending",
+		layout_config = {
+			horizontal = {
+				prompt_position = "top",
+				preview_width = 0.55,
+				results_width = 0.8,
+			},
+			vertical = {
+				mirror = false,
+			},
+			width = 0.87,
+			height = 0.80,
+			preview_cutoff = 120,
+		},
+	},
 })
 pcall(ts.load_extension, "fzf")
 pcall(ts.load_extension, "ui-select")
 
--- [[ToggleTerm]]
-require("toggleterm").setup({
-  shade_terminals = false,
-  shell = vim.o.shell,
-  direction = "horizontal",
-})
-
-function NewTerminal()
-  local buffers = vim.api.nvim_list_bufs()
-  local count = 0
-  for _, buf in ipairs(buffers) do
-    local buf_name = vim.api.nvim_buf_get_name(buf)
-    if buf_name:find("toggleterm#") then
-      count = count + 1
-    end
-  end
-  count = count + 1
-  vim.cmd(count .. "ToggleTerm direction=horizontal")
-end
-
 --[[Visual Aid]]
 function Toggle_Wrap()
-  if vim.o.wrap then
-    vim.o.textwidth = 0
-    vim.o.wrap = false
-    vim.o.colorcolumn = ""
-  else
-    vim.o.textwidth = 120
-    vim.o.wrap = true
-    vim.o.colorcolumn = "121"
-  end
+	if vim.o.wrap then
+		vim.o.textwidth = 0
+		vim.o.wrap = false
+		vim.o.colorcolumn = ""
+	else
+		vim.o.textwidth = 120
+		vim.o.wrap = true
+		vim.o.colorcolumn = "121"
+	end
 end
 
 function Toggle_Spell_Check()
-  local buf = vim.api.nvim_get_current_buf()
-  local new_val = not vim.api.nvim_get_option_value("spell", { buf = buf })
-  vim.api.nvim_set_option_value("spell", new_val, { buf = buf })
+	local buf = vim.api.nvim_get_current_buf()
+	local new_val = not vim.api.nvim_get_option_value("spell", { buf = buf })
+	vim.api.nvim_set_option_value("spell", new_val, { buf = buf })
 end
