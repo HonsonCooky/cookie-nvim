254e31443c9eda39875eaa3d62d7c936dd9802b5
Harrison <harrison.cook@z.co.nz>
2024-05-21 12:14:32 +1200

    remove italics

--- init.lua
+++ init.lua
@@ -426,6 +426,7 @@ require("lazy").setup({
         "rose-pine/neovim",
         name = "rose-pine",
         config = function()
+            require("rose-pine").setup({ styles = { italic = false } })
             vim.cmd.colorscheme("rose-pine")
         end
     }
