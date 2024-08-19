3e0b4945412e285cbc17dd679555d34dc23daeb7
Harrison Cook <HonsonCooky@gmail.com>
2024-08-19 15:50:47 +1200

    Attempt to make LSPs

---
+++ lua/plugins/theme.lua
@@ -0,0 +1,21 @@
+﻿return {
+    "catppuccin/nvim",
+    name = "catppuccin",
+    priority = 1000,
+    dependencies = {
+        "f-person/auto-dark-mode.nvim",
+    },
+    config = function()
+        require("catppuccin").setup({ no_italic = true })
+        vim.cmd.colorscheme("catppuccin")
+        require("auto-dark-mode").setup({
+            update_interval = 1000,
+            set_dark_mode = function()
+                vim.o.background = "dark"
+            end,
+            set_light_mode = function()
+                vim.o.background = "light"
+            end,
+        })
+    end,
+}
